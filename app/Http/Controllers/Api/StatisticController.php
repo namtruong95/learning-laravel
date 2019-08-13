1f75ad0b66e92afd4e2c02d0a803d4479d93c512
Nam Truong <nam.tt@neo-lab.vn>
2019-08-13 13:28:29 +0700

    fix lint

--- app/Http/Controllers/Api/StatisticController.php
+++ app/Http/Controllers/Api/StatisticController.php
@@ -2,7 +2,6 @@
 
 namespace App\Http\Controllers\Api;
 
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
 use App\ClassRoom;
@@ -35,26 +34,26 @@ class StatisticController extends Controller
 
     public function statisticUsingEloQuent()
     {
-        $data = ClassRoom::leftJoin('students', function($join) {
-                $join->on('students.class_room_id', '=', 'class_rooms.id');
-            })
-            ->leftJoin('students_subject_classes', function($join) {
-                $join->on('students_subject_classes.student_id', '=', 'students.id');
-            })
-            ->leftJoin('subject_classes', function($join) {
-                $join->on('students_subject_classes.subject_class_id', '=', 'subject_classes.id');
-            })
-            ->leftJoin('subjects', function($join) {
-                $join->on('subjects.id', '=', 'subject_classes.subject_id');
-            })
-            ->select('class_rooms.id', 'class_rooms.name')
-            ->selectRaw('count(subjects.id) AS total_subject')
-            ->selectRaw('sum(subjects.number_of_credits) AS total_credits')
-            ->selectRaw('(SELECT count(id) FROM students WHERE students.class_room_id = class_rooms.id) AS total_students')
-            ->groupBy('class_rooms.id')
-            ->groupBy('class_rooms.name')
-            ->orderBy('class_rooms.id')
-            ->get();
+        $data = ClassRoom::leftJoin('students', function ($join) {
+            $join->on('students.class_room_id', '=', 'class_rooms.id');
+        })
+        ->leftJoin('students_subject_classes', function ($join) {
+            $join->on('students_subject_classes.student_id', '=', 'students.id');
+        })
+        ->leftJoin('subject_classes', function ($join) {
+            $join->on('students_subject_classes.subject_class_id', '=', 'subject_classes.id');
+        })
+        ->leftJoin('subjects', function ($join) {
+            $join->on('subjects.id', '=', 'subject_classes.subject_id');
+        })
+        ->select('class_rooms.id', 'class_rooms.name')
+        ->selectRaw('count(subjects.id) AS total_subject')
+        ->selectRaw('sum(subjects.number_of_credits) AS total_credits')
+        ->selectRaw('(SELECT count(id) FROM students WHERE students.class_room_id = class_rooms.id) AS total_students')
+        ->groupBy('class_rooms.id')
+        ->groupBy('class_rooms.name')
+        ->orderBy('class_rooms.id')
+        ->get();
 
         // dd(DB::getQueryLog());
 
